235f21625d0c95bf7201d1cb18ca9426e6cb4084
MetalSnow <yasuotrend@gmail.com>
2024-02-03 15:32:26 +0800

    Fix bug in numTwo assignment within the click event listener.
    Previously, numTwo stored the value from displayDiv.textContent with extra characters (plus sign and spaces), but now it correctly stores only the numeric values.

--- main.js
+++ main.js
@@ -13,43 +13,75 @@ function divide(a, b) {
   return a / b;
 }
 
-//var
-let numOne = 1;
-let operator = "*";
-let numTwo = 1;
-
-let result;
-function operate() {
-  switch (operator) {
-    case "+":
-      result = add(numOne, numTwo);
-      break;
-    case "-":
-      result = subtract(numOne, numTwo);
-      break;
-    case "*":
-      result = multiply(numOne, numTwo);
-      break;
-    case "/":
-      result = divide(numOne, numTwo);
-      break;
-  }
-}
-operate();
-
-console.log(result);
-
 let displayDiv = document.querySelector(".display");
 let numBtn = document.querySelector(".numbers");
+let operators = document.querySelector(".operators");
+let equals = document.querySelector("#equals");
 
-let value = "";
+// Numbers section
+let numOne = "";
+let numTwo = "";
 
 numBtn.addEventListener("click", (event) => {
   let isButton = event.target.nodeName === "BUTTON";
   if (!isButton) {
     return;
+  } else if (displayDiv.textContent.startsWith(" ")) {
+    displayDiv.textContent += event.target.textContent;
+    numTwo = displayDiv.textContent.slice(3);
   } else {
-    value += event.target.textContent;
+    displayDiv.textContent += event.target.textContent;
+    numOne = displayDiv.textContent;
   }
-  displayDiv.textContent = value;
+  console.log(numTwo);
+});
+
+// Operator Section
+let operator = "";
+operators.addEventListener("click", (event) => {
+  let id = event.target.id;
+  let isButton = event.target.nodeName === "BUTTON";
+
+  if (!isButton) {
+    return;
+  }
+
+  switch (id) {
+    case "add":
+      displayDiv.textContent = " + ";
+      break;
+    case "subtract":
+      displayDiv.textContent = " – ";
+      break;
+    case "multiply":
+      displayDiv.textContent = " × ";
+      break;
+    case "divide":
+      displayDiv.textContent = " ÷ ";
+      break;
+  }
+  operator = displayDiv.textContent;
+  console.log(operator);
+});
+
+// Operate Section
+let result;
+function operate() {
+  switch (operator) {
+    case " + ":
+      result = add(numOne, numTwo);
+      break;
+    case " – ":
+      result = subtract(numOne, numTwo);
+      break;
+    case " × ":
+      result = multiply(numOne, numTwo);
+      break;
+    case " ÷ ":
+      result = divide(numOne, numTwo);
+      break;
+  }
+}
+equals.addEventListener("click", () => {
+  console.log(operate());
 });
