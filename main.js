b51a31be94a5aa6f7308dc412608667805dd9a7e
MetalSnow <yasuotrend@gmail.com>
2024-02-03 22:08:18 +0800

    Update switch cases by adjusting operator values.  Enhance the clear button functionality to reset numOne, numTwo, and the operator to empty strings.

--- main.js
+++ main.js
@@ -13,10 +13,12 @@ function divide(a, b) {
   return a / b;
 }
 
+// Get buttons node
 let displayDiv = document.querySelector(".display");
 let numBtn = document.querySelector(".numbers");
 let operators = document.querySelector(".operators");
 let equals = document.querySelector("#equals");
+let clear = document.querySelector("#clear");
 
 // Numbers section
 let numOne = "";
@@ -26,12 +28,15 @@ numBtn.addEventListener("click", (event) => {
   let isButton = event.target.nodeName === "BUTTON";
   if (!isButton) {
     return;
-  } else if (displayDiv.textContent.startsWith(" ")) {
+  } else if (operator !== "") {
+    displayDiv.textContent = numTwo;
     displayDiv.textContent += event.target.textContent;
-    numTwo = displayDiv.textContent.slice(3);
+    numTwo = displayDiv.textContent;
+    console.log("im num Two: ", numTwo);
   } else {
     displayDiv.textContent += event.target.textContent;
     numOne = displayDiv.textContent;
+    console.log("im num one: ", numOne);
   }
 });
 
@@ -40,49 +45,55 @@ let operator = "";
 operators.addEventListener("click", (event) => {
   let id = event.target.id;
   let isButton = event.target.nodeName === "BUTTON";
-
   if (!isButton) {
     return;
   }
 
   switch (id) {
     case "add":
-      displayDiv.textContent = " + ";
+      operator = "+";
       break;
     case "subtract":
-      displayDiv.textContent = " – ";
+      operator = "-";
       break;
     case "multiply":
-      displayDiv.textContent = " × ";
+      operator = "*";
       break;
     case "divide":
-      displayDiv.textContent = " ÷ ";
+      operator = "/";
       break;
   }
-  operator = displayDiv.textContent;
-  console.log(operator);
 });
 
 // Operate Section
 let result = "";
 function operate() {
   switch (operator) {
-    case " + ":
+    case "+":
       result = add(+numOne, +numTwo);
       break;
-    case " – ":
+    case "-":
       result = subtract(+numOne, +numTwo);
       break;
-    case " × ":
+    case "*":
       result = multiply(+numOne, +numTwo);
       break;
-    case " ÷ ":
+    case "/":
       result = divide(+numOne, +numTwo);
       break;
   }
   return result;
 }
+
 equals.addEventListener("click", () => {
-  displayDiv.textContent = operate();
-  numOne = displayDiv.textContent;
+  operate();
+  displayDiv.textContent = result;
+});
+
+clear.addEventListener("click", () => {
+  numOne = "";
+  numTwo = "";
+  operator = "";
+  displayDiv.textContent = "";
+  console.log("cleared");
 });
